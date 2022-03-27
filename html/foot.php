d584a696369340aae11e4055eff6394e606fb958
Izul Wahidin <izul@wahid.in>
2022-03-28 01:28:35 +0700

    Update foot.php

--- html/foot.php
+++ html/foot.php
@@ -6,17 +6,22 @@
     </footer>
     <script src="<?=DOMAIN?>/html/assets/js/bootstrap.bundle.min.js"></script>
     <script>
-        localStorage.setItem('w'+sha1(window.location.href),'true')  
-        document.querySelectorAll('#check_eps').forEach(v => {
-          if(localStorage.getItem('w'+sha1(v.href)) == 'true'){
-            if(v.childNodes[1].localName == 'span'){
-              v.childNodes[1].textContent = 'Watched';
-            }else if(v.childNodes[5].localName == 'p'){
-              v.childNodes[5].textContent = 'Watched';
-              }
-            }else{
+    document.querySelectorAll('#check_eps').forEach(v => {
+      v.addEventListener('click', () => {
+        localStorage.setItem('w'+sha1(v.href),'true');
+      });
+
+      if(localStorage.getItem('w'+sha1(v.href)) == 'true'){
+        if(v.childNodes[1].localName == "span"){
+          // console.log('>> childNodes 1 <<');
+          v.childNodes[1].textContent = 'Watched';
+        }
+        else if(v.childNodes[5].localName == 'p'){
+          // console.log('>> childNodes 5 <<');
+          v.childNodes[5].textContent = 'Watched';
           }
-        });
+        }
+    });
     </script>
   </body>
 </html>
