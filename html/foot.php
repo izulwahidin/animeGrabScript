ddff3cddfa0c3278ec0e74f17233a6af5da8c09f
Izul Wahidin <izul@wahid.in>
2022-03-27 23:42:10 +0700

    Update foot.php

--- html/foot.php
+++ html/foot.php
@@ -5,5 +5,18 @@
       </div>
     </footer>
     <script src="<?=DOMAIN?>/html/assets/js/bootstrap.bundle.min.js"></script>
+    <script>
+        localStorage.setItem('w'+sha1(window.location.href),'true')  
+        document.querySelectorAll('#check_eps').forEach(v => {
+          if(localStorage.getItem('w'+sha1(v.href)) == 'true'){
+            if(v.childNodes[1].localName == 'span'){
+              v.childNodes[1].textContent = 'Watched';
+            }else if(v.childNodes[5].localName == 'p'){
+              v.childNodes[5].textContent = 'Watched';
+              }
+            }else{
+          }
+        });
+    </script>
   </body>
 </html>
